85cd9db7042af621465bfda5f5a95a4e0f7ad7b4
김재우 <ksg3477@naver.com>
2023-06-11 16:57:36 +0900

    QueryDSL 세팅

---
+++ src/test/java/study/querydsl/entity/HelloTest.java
@@ -0,0 +1,14 @@
+package study.querydsl.entity;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class HelloTest {
+
+
+
+}
